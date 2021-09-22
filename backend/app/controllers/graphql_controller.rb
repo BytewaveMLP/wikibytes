a6a947d3be3dd4e4f5481328c045633ada4947b0
Eliot Partridge <byte@code.horse>
2021-09-21 19:31:14 -0500

    Setup RSpec/Rubocop, cleanup generated code

--- backend/app/controllers/graphql_controller.rb
+++ backend/app/controllers/graphql_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GraphqlController < ApplicationController
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
@@ -14,12 +16,9 @@ class GraphqlController < ApplicationController
     }
     result = BackendSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
-  rescue StandardError => e
-    raise e unless Rails.env.development?
-    handle_error_in_development(e)
   end
 
-  private
+private
 
   # Handle variables in form data, JSON body, or a blank value
   def prepare_variables(variables_param)
@@ -39,12 +38,5 @@ class GraphqlController < ApplicationController
     else
       raise ArgumentError, "Unexpected parameter: #{variables_param}"
     end
-  end
-
-  def handle_error_in_development(e)
-    logger.error e.message
-    logger.error e.backtrace.join("\n")
-
-    render json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }, status: 500
   end
 end
